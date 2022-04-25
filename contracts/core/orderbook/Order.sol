65160135faa2a2a0d713e7b8630e3a74b073b7d2
cherideal <101du@163.com>
2022-04-25 22:36:24 +0800

    ORDER

--- contracts/core/orderbook/Order.sol
+++ contracts/core/orderbook/Order.sol
@@ -2,31 +2,33 @@
 pragma solidity ^0.8.0;
 
 import "../../deps/access/Ownable.sol";
-import "./interfaces/IOrder.sol";
 import "../../deps/ERC721.sol";
 import "../../deps/extensions/ERC721Enumerable.sol";
 import "../../deps/extensions/ERC721Burnable.sol";
 import "../../deps/access/AccessControlEnumerable.sol";
 import "../../deps/utils/Context.sol";
 import "../../deps/utils/Counters.sol";
+import "../../deps/libraries/SafeMath.sol";
 
 contract Order is
     Context,
     AccessControlEnumerable,
     ERC721Enumerable,
     ERC721Burnable,
-    IOrder,
     Ownable {
 
+    event OrderUpdate(uint256 indexed tokenId, uint256 offer1, uint256 remain1, uint256 offer2, uint256 remain2);
+
     struct OrderDetail {
-        uint _price;
-        uint _offer;
-        uint _remain;
+        uint256 _price;
+        uint256 _offer;
+        uint256 _remain;
         uint8 _type;
         uint8 _index;
     }
 
     using Counters for Counters.Counter;
+    using SafeMath for uint256;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
@@ -54,8 +56,35 @@ contract Order is
     }
 
     function burn(uint256 tokenId) public virtual override {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ORDER: must have minter role to burn");
         delete(getOrderDetail[tokenId]);
-        super.burn(tokenId);
+        _burn(tokenId);
+    }
+
+    function add(uint256 tokenId, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ORDER: must have minter role to deposit");
+        OrderDetail memory orderDetail = getOrderDetail[tokenId];
+        require(orderDetail._price != 0, "ORDER: order must be exist");
+        (uint256 offer, uint256 remain) = (orderDetail._offer.add(amount), orderDetail._remain.add(amount));
+
+        emit OrderUpdate(tokenId, orderDetail._offer, orderDetail._remain, offer, remain);
+
+        getOrderDetail[tokenId]._offer = offer;
+        getOrderDetail[tokenId]._remain = remain;
+    }
+
+    function sub(uint256 tokenId, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ORDER: must have minter role to deposit");
+        OrderDetail memory orderDetail = getOrderDetail[tokenId];
+        require(orderDetail._price != 0, "ORDER: order must be exist");
+        uint256 remain = orderDetail._remain.sub(amount);
+        if (remain == 0) {
+            this.burn(tokenId);
+        }
+        else {
+            emit OrderUpdate(tokenId, orderDetail._offer, orderDetail._remain, orderDetail._offer, remain);
+            getOrderDetail[tokenId]._remain = remain;
+        }
     }
 
     function _beforeTokenTransfer(
